04cf5bfd7a6771d646155a5b511848d49bcd23bd
Flagship 2 <flagship2@charlestoncodes.com>
2012-07-11 19:12:48 -0400

    First major accomplishment, got everything to pass

---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,23 @@
+Given /^I am on the home page$/ do
+    visit '/'
+end
+
+Then /^I should see "(.*?)"$/ do |arg1|
+    page.should have_content(arg1)
+end
+
+Given /^I visit "(.*?)"$/ do |url|
+    visit url
+end
+
+When /^I fill in "(.*?)" with "(.*?)"$/ do |field, value|
+    fill_in field, with: value
+end
+
+When /^I click "(.*?)"$/ do |text|
+    click_button text
+end
+
+Then /^I should not see "(.*?)"$/ do |arg1|
+  page.should_not have_content(arg1)
+end
